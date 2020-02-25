e8268611bf8931283d21766d05f963ecdf270a24
siwar2019 <siwarbougrine@gmail.com>
2020-02-25 17:46:23 +0100

    init commit

---
+++ Components/StreamingScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  Button,
+  View,
+} from 'react-native';
+
+export default class StreamingScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+    <View><Text>streaming</Text></View>
+    );
+  }
+}
+
+
+
